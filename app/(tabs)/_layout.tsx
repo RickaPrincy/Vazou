ccbec1537104ee83b41e701c585dc6f77309b3e5
Eclisher <hei.harizo@gmail.com>
2025-03-17 11:34:45 +0300

    feat: add the bouttonBarList

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -25,16 +25,21 @@ const TabLayout = () => {
       <Tabs
         screenOptions={{
           tabBarStyle: {
-            height: 65,
-            marginLeft: 8,
-            marginRight: 8,
-            borderRadius: 20,
-            marginBottom: 8,
+            marginBottom: 10,
+            marginLeft: 6,
+            marginRight: 5,
+            height: 60,
+            borderRadius: 35,
             backgroundColor: palette.card,
+            shadowColor: '#000',
+            shadowOpacity: 0.1,
+            shadowRadius: 10,
+            shadowOffset: { width: 0, height: 4 },
+            elevation: 5,
             borderColor: palette.card,
           },
           headerShown: false,
-          tabBarIconStyle: { marginTop: 5 },
+          tabBarIconStyle: { marginTop: 10 },
           tabBarActiveTintColor: palette.primary,
           tabBarInactiveTintColor: palette.secondary,
         }}
@@ -42,46 +47,63 @@ const TabLayout = () => {
         <Tabs.Screen
           name="playlist/index"
           options={{
-            title: 'PlayList',
-            tabBarIcon: ({ color, size }: { color: string; size: number }) => (
-              <Feather name="list" size={size} color={color} />
+            title: '',
+            tabBarIcon: ({ color }) => (
+              <Feather name="list" size={22} color={color} />
             ),
           }}
         />
         <Tabs.Screen
           name="search"
           options={{
-            title: 'Search',
-            tabBarIcon: ({ color, size }: { color: string; size: number }) => (
-              <Feather name="search" size={size} color={color} />
+            title: '',
+            tabBarIcon: ({ color }) => (
+              <Feather name="search" size={22} color={color} />
             ),
           }}
         />
         <Tabs.Screen
           name="index"
           options={{
-            title: 'Home',
-            headerShown: false,
-            tabBarIcon: ({ color, size }: { color: string; size: number }) => (
-              <Feather name="home" size={size} color={color} />
+            title: '',
+            tabBarIcon: ({ color, focused }) => (
+              <View
+                style={{
+                  width: 65,
+                  height: 65,
+                  backgroundColor: focused
+                    ? palette.primary
+                    : palette.secondary,
+                  borderRadius: 35,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  shadowColor: '#000',
+                  shadowOpacity: 0.2,
+                  shadowRadius: 10,
+                  shadowOffset: { width: 0, height: 4 },
+                  marginBottom: 20,
+                }}
+              >
+                <Feather name="home" size={25} color="#fff" />
+              </View>
             ),
           }}
         />
         <Tabs.Screen
           name="url"
           options={{
-            title: 'Notifications',
-            tabBarIcon: ({ color, size }: { color: string; size: number }) => (
-              <Feather name="bell" size={size} color={color} />
+            title: '',
+            tabBarIcon: ({ color }) => (
+              <Feather name="bell" size={22} color={color} />
             ),
           }}
         />
         <Tabs.Screen
           name="settings"
           options={{
-            title: 'Settings',
-            tabBarIcon: ({ color, size }: { color: string; size: number }) => (
-              <Feather name="settings" size={size} color={color} />
+            title: '',
+            tabBarIcon: ({ color }) => (
+              <Feather name="settings" size={22} color={color} />
             ),
           }}
         />
